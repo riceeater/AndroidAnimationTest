e9abe43bd4d0bca82a4be58551294b3bdc603d34
xiaomili <xiaomi987@hotmail.com>
2018-04-13 15:12:51 +0800

    属性动画执行流程

---
+++ app/src/main/java/com/xylitolz/androidanimationtest/PropertyAnalysisActivity.java
@@ -0,0 +1,11 @@
+package com.xylitolz.androidanimationtest;
+
+/**
+ * @author 小米Xylitol
+ * @email xiaomi987@hotmail.com
+ * @desc
+ * @date 2018-04-13 12:53
+ */
+
+public class PropertyAnalysisActivity {
+}
